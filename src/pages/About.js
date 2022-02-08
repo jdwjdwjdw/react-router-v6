592b8a89e6023cd13ca1d0f5b2e0e6dfaac5df49
jdwjdwjdw <jacobdeanwilson@gmail.com>
2022-02-07 16:42:01 -0800

    Update nested routes for v6

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Route, useNavigate } from "react-router-dom"
+import { Route, useNavigate, Routes } from "react-router-dom"
 
 // nested routes
 import Offers from "./Offers"
@@ -14,9 +14,9 @@ export default function About() {
 
       <button onClick={() => navigate('/products')}>See our products</button>
 
-      {/* <Route path="/about/offers">
-        <Offers />
-      </Route> */}
+      <Routes>
+        <Route path="offers" element={<Offers />} />
+      </Routes>
     </div>
   )
 }
